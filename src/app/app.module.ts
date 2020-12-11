1763fd8ecf222611c046bbcd1c80ad29327cf555
7ishani <ishanidilu@gmail.com>
2020-12-11 15:18:24 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUserComponent } from './components/user/list-user/list-user.component';
+import { AddUserComponent } from './components/user/add-user/add-user.component';
+import { EditUserComponent } from './components/user/edit-user/edit-user.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
+const appRoutes = [
+  {path:'', component:LandingPageComponent},
+  {path:'listuser', component:ListUserComponent},
+  {path:'adduser', component:AddUserComponent},
+  {path:'edituser/:id', component:EditUserComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUserComponent,
+    AddUserComponent,
+    EditUserComponent,
+    NavBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
